5ff8170eb0b5185c641ae897d95e68a16150d36a
MozartMongi <71974220+MozartMongi@users.noreply.github.com>
2026-09-16 16:53:45 +0700

    Change access denied link to '/lowongan'

--- src/components/company/company-access-denied.tsx
+++ src/components/company/company-access-denied.tsx
@@ -25,7 +25,7 @@ export function CompanyAccessDenied({ role }: CompanyAccessDeniedProps) {
       </p>
       <p className="mt-8 flex flex-col gap-3 sm:flex-row sm:flex-wrap">
         <Link
-          href="/"
+          href="/lowongan"
           className={buttonVariants({
             variant: "outline",
             size: "lg",
